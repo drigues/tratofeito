3af59c33bdab78e4165b533af9a6fe70a7d9291d
Thiago Rodrigues Oliveira <thiagorodrigues@Mac.Home>
2025-08-07 00:15:04 +0100

    render docker files

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Start PHP-FPM in background
-php-fpm -D
+php-fpm &
+
+# Aguarda uns segundos para o PHP-FPM iniciar corretamente (opcional, mas seguro)
+sleep 3
 
 # Run Laravel setup
 php artisan config:cache
@@ -9,5 +12,5 @@ php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
-# Start Nginx in foreground
+# Start Nginx in foreground (importante: não deve sair ou o container para)
 nginx -g "daemon off;"
