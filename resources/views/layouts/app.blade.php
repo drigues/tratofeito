7351a922aa5913e36390e45fbe959cbd84d750d3
Thiago Rodrigues Oliveira <thiagorodrigues@Mac.Home>
2025-08-07 22:11:48 +0100

    vite config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt">
 <head>
-  <meta charset="UTF-8">
-  <title>@yield('title', 'Trato Feito')</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ $title ?? 'Trato Feito' }}</title>
+
+  {{-- Vite: entradas DEVEM corresponder ao vite.config.js --}}
   @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-white text-gray-900 antialiased">
   @yield('content')
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/scss/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+  },
 });
