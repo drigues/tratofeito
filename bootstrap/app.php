56343d7d3ebbe4e5dce958b751dbc1d3ad090fcb
Thiago Rodrigues Oliveira <thiagorodrigues@Mac.Home>
2025-08-08 07:59:35 +0100

    vite app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Vite;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withVite(function (Vite $vite) {
+        // We build into "public/build" and the manifest is ".vite/manifest.json"
+        $vite->useBuildDirectory('build');
+        $vite->useManifestFilename('.vite/manifest.json');
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
